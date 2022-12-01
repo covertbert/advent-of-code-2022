7e0869160a31527b4819c337bda6bcb1f7eb3c1b
Bertie Blackman <blackmanrgh@gmail.com>
2022-12-01 15:56:17 +0000

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import('@types/prettier').Options} */
+
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+}
